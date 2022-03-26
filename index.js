2e92d73c37b596ac386b74444d9358a6ff43b17d
Alexi <71225531+Alexi543@users.noreply.github.com>
2022-03-26 16:09:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 const buttons = document.querySelectorAll(".btn");
 
-const randrange = (min = 0, max) =>
-{
+const randrange = (min = 0, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const getInputAll = () => 
-{
-    for (let i = 0; i < data.inputs.path.length; i++)
-    {
+const getInputAll = () => {
+    for (let i = 0; i < data.inputs.path.length; i++) {
         data[data.inputs.path[i]].value = clearInput(data.inputs.variables[i].value);
     }
 
     checkInput();
 }
 
-const clearInput = (input) => 
-{
+const clearInput = (input) => {
     input = String(input);
 
-    if (input !== "" && input !== undefined) 
-    {
+    if (input !== "" && input !== undefined) {
         input = String(input).split("");
         input = input.filter(x => data.digits.indexOf(x) !== -1);
 
-        if (input !== []) 
-        {
+        if (input !== []) {
             input = String(input);
 
-            for (let i = 0; i < input.split(",").length--; i++)
-            {
+            for (let i = 0; i < input.split(",").length--; i++) {
                 input = input.replace(",", "");
             }
 
@@ -40,54 +33,42 @@ const clearInput = (input) =>
     return;
 }
 
-const checkInput = () =>
-{
-    data.inputs.path.forEach(path =>
-    {
+const checkInput = () => {
+    data.inputs.path.forEach(path => {
         let input = data[path];
 
-        if (input.value === undefined)
-        {
+        if (input.value === undefined) {
             input.value = input.default;
         }
     })
 }
 
-const start = () =>
-{
-    for (let input = 0; input < data.inputs.variables.length; input++)
-    {
+const start = () => {
+    for (let input = 0; input < data.inputs.variables.length; input++) {
         data.inputs.variables[input].value = data[data.inputs.path[input]].default;
     }
 }
 
-buttons.forEach(button => 
-{
-    button.addEventListener("click", Event => 
-    {
-        if (button.id === "start") 
-        {
+buttons.forEach(button => {
+    button.addEventListener("click", Event => {
+        if (button.id === "start") {
             getInputAll();
 
             output.innerHTML = "<p>Generate</p>";
 
             let allPassWords = "";
 
-            for (let passWordGen = 1; passWordGen <= data.passWords.value; passWordGen++)
-            {  
+            for (let passWordGen = 1; passWordGen <= data.passWords.value; passWordGen++) {  
                 let passWord = [];
                 let out = "";
                 
-                data.inputs.caraVals.forEach(path =>
-                {
+                data.inputs.caraVals.forEach(path => {
                     let caraters = data[path];
     
-                    for (let i = 0; i < caraters.value; i++)
-                    {
+                    for (let i = 0; i < caraters.value; i++) {
                         let letter;
     
-                        do 
-                        {
+                        do {
                             letter = caraters.list[randrange(0, caraters.list.length - 1)];
                         } while (letter === undefined);
     
@@ -97,8 +78,7 @@ buttons.forEach(button =>
     
                 passWord = passWord.sort(() => Math.random() - 0.5);
                 
-                passWord.forEach(carater => 
-                {
+                passWord.forEach(carater => {
                     out += carater;
                 })
 
@@ -107,10 +87,8 @@ buttons.forEach(button =>
 
             output.innerHTML = allPassWords;
         }
-        else if (button.id === "clear-all")
-        {
-            for (let input = 0; input < data.inputs.variables.length; input++)
-            {
+        else if (button.id === "clear-all") {
+            for (let input = 0; input < data.inputs.variables.length; input++) {
                 data.inputs.variables[input].value = data[data.inputs.path[input]].default;
             }
 
